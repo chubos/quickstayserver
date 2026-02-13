35439ac1126b3b797aa93e4f135d1f044b55ae99
Hubert Lech <hubertlech@icloud.com>
2026-02-13 12:17:28 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
+
+connectDB();
+
+const app = express();
+app.use(cors()); // Enable CORS (Cross Origin Resource Sharing) for all routes
+
+//Middleware
+app.use(express.json()); // Parse JSON bodies
+app.use(clerkMiddleware());
+
+// API to listen for Clerk webhooks
+app.use("/api/clerk", clerkWebhooks);
+app.get("/", (req, res) => res.send("Hello World! API is working."));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

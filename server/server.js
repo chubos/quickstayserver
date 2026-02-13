8442ff701f7d4ca2423cb39d79102957b2133909
Hubert Lech <hubertlech@icloud.com>
2026-02-13 12:38:44 +0100

    change

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(clerkMiddleware());
 
 // API to listen for Clerk webhooks
 app.use("/api/clerk", clerkWebhooks);
-app.get("/", (req, res) => res.send("Hello World! API is working."));
+app.get("/", (req, res) => res.send("Hello World! API is workingg."));
 
 const PORT = process.env.PORT || 3000;
 
